cf955a7ce7f86d40388f2e733ceb07610641b1ae
MAarsman <magdaalidia@gmail.com>
2017-11-07 09:16:18 +0100

    First test and setting project

---
+++ src/main/java/com/epam/wallet/Dollar.java
@@ -0,0 +1,16 @@
+package com.epam.wallet;
+
+public class Dollar {
+
+    int amount;
+
+    Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+
+    }
+
+
+}
---
+++ src/test/java/com/epam/wallet/WalletTest.java
@@ -0,0 +1,14 @@
+package com.epam.wallet;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class WalletTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
