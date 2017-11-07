7a98a0be38a007902486419d19bb02b6c2a2b4ef
MAarsman <magdaalidia@gmail.com>
2017-11-07 09:40:28 +0100

    testEquality

--- src/test/java/com/epam/wallet/WalletTest.java
+++ src/test/java/com/epam/wallet/WalletTest.java
@@ -1,7 +1,11 @@
 package com.epam.wallet;
 
 import org.junit.Test;
+
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class WalletTest {
 
@@ -12,6 +16,12 @@ public class WalletTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
 }
