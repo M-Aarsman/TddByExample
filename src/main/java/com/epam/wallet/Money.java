cc8de5643e3f5b4b6ee84dd7cc1e3b7e2afa5232
MAarsman <magdaalidia@gmail.com>
2017-11-07 10:49:10 +0100

    common equality

--- src/main/java/com/epam/wallet/Money.java
+++ src/main/java/com/epam/wallet/Money.java
@@ -1,4 +1,11 @@
 package com.epam.wallet;
 
 public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals (Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
